90c9eeb21b4e7837a5bf46a1ff3a560f7cac057b
Victor Millan <victormillan23aguilar@gmail.com>
2025-08-12 00:51:33 -0600

    Arreglo de seguridad para la base de datos

--- app.py
+++ app.py
@@ -3,13 +3,23 @@ from routes.Ajuste_Tolerancia import tolerancia
 from routes.Analisis_Similitud import analisis
 from config.config import db
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 CORS(app)
 
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
 app.secret_key = 'secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/verano_cientifico2'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_password}@{db_host}/{db_name}'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/verano_cientifico2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
